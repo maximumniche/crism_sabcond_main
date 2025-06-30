d48c8e0324b6602caa579d913aa5dd3c5533f15f
Imad K <imadaman99@gmail.com>
2025-06-30 15:44:26 -0400

    Added stuff to mlx file and files so that code works

--- startup_addpath.m
+++ startup_addpath.m
@@ -1,5 +1,6 @@
 function [] = startup_addpath()
-toolbox_root_dir = '/Users/itohy1/src/matlab/toolbox/';
+toolbox_root_dir = '/home/imadk/Documents/MATLAB/CRISM/toolbox';
+data_root_dir = '/home/imadk/Documents/MATLAB/CRISM/data';
 
 addpath(fullfile(toolbox_root_dir,'base'));
 addpath(fullfile(toolbox_root_dir,'sc'));
@@ -7,12 +8,20 @@ addpath(fullfile(toolbox_root_dir,'export_fig'));
 addpath(fullfile(toolbox_root_dir,'relab_toolbox'));
 addpath(fullfile(toolbox_root_dir,'optProp'));
 addpath(fullfile(toolbox_root_dir,'tetracorder'));
+
 % addpath('/Users/itohy1/data/MCD/MCD_v5_3/MCD5.3/mcd/matlab/',...
 %         fullfile(toolbox_root_dir,'mcd_toolbox/'));
-addpath('/Users/itohy1/data/MCD/MCD_v6_1/MCD_6.1/mcd/interfaces/matlab',...
-        '/Users/itohy1/data/MCD/MCD_v5_3/MCD5.3/mcd/matlab/', ...
-        fullfile(toolbox_root_dir,'mcd_toolbox/'));
-addpath(fullfile(toolbox_root_dir,'quaternions-1.3/quaternions/'));
+
+% reference for mcd data:
+% addpath('/Users/itohy1/data/MCD/MCD_v6_1/MCD_6.1/mcd/interfaces/matlab',...
+%        '/Users/itohy1/data/MCD/MCD_v5_3/MCD5.3/mcd/matlab/', ...
+%        fullfile(toolbox_root_dir,'mcd_toolbox/'));
+
+addpath(fullfile(data_root_dir, 'MCD_6.1/mcd/interfaces/matlab'),...
+        fullfile(toolbox_root_dir, 'mcd_toolbox'));
+
+% addpath(fullfile(toolbox_root_dir,'quaternions-1.3/quaternions/'));
+
 addpath(fullfile(toolbox_root_dir,'crism_conv_hitran'));
 
 run(fullfile(toolbox_root_dir,'envi','envi_startup_addpath'));
